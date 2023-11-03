861ec77920bf0f3c3e8f4b766223430881c94039
Under97 <66751151+Underplayer97@users.noreply.github.com>
2023-11-03 11:54:51 -0400

    Fixed Enchantment Weight

--- data.js
+++ data.js
@@ -258,21 +258,21 @@ var data = {
 
         abrasion: {
             levelMax: "2",
-            weight: "2",
+            weight: "1",
             incompatible: [],
             items: ["sword","axe"]
         },
         
         adrenaline: {
             levelMax: "3",
-            weight: "3",
+            weight: "1",
             incompatible: [],
             items: ["shield"]
         },
         
         arcane_defence: {
             levelMax: "5",
-            weight: "4",
+            weight: "1",
             incompatible: [],
             items: ["helmet", "chestplate", "leggings", "boots","turtle_shell"]
         },
